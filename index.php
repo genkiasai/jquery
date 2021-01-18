4d373fa582f31183b71dd311f5ae627bb043cc75
genkiasai <baseballboy1025@gmail.com>
2021-01-18 17:57:57 +0900

    1-2

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>jQuery</title>
+</head>
+<body>
+    <h1 id="title">タイトル１</h1>
+    <p id="element1">要素１</p>
+    <script src="./jquery-3.5.1.min.js">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    </script>
+</body>
+</html>
+
+<!-- 
+jQueryの基本（ライブラリの読み込み）
+・書き方
+　1．ライブラリを読み込む（インクルードする）
+　2．jQueryが提供する機能を使ってDOMを操作する
+
+・ライブラリの読み込み
+　－　ダウンロードしてパスを指定する
+　－　CDN（Contents Distribution Network）のURLを指定する
+　　－　読み込みが早くなる場合がある
+
+
+
+ -->
