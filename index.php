f6817bc484b357260b0b5ae793a9165bbd38b659
genkiasai <baseballboy1025@gmail.com>
2021-01-19 06:31:25 +0900

    2-3

--- index.php
+++ index.php
@@ -10,11 +10,12 @@
         <h1 id="title1" class="title">コンパイル言語</h1>
         <ul>
             <li id="java">Java</li>
-            <li id="c">C</li>
+            <li id="c">C
                 <ul>
                     <li id="cpp">C++</li>
                     <li id="cs">C#</li>
                 </ul>
+            </li>
             <li id="objective-c">ObjectiveC</li>
         </ul>
     </div>
@@ -31,35 +32,37 @@
     <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
     <script src="./jquery-3.5.1.min.js"></script>
     <script>
-        $(function(){
-        // －　（スペース）
-        // ある要素の配下の要素
-        // $("#compile ul").css("color", "red");
-        // －　>
-        // ある要素の直下の要素
-        // 上の例ではCの下が除かれる
-        // $("#compile > ul").css("color", "blue");
-        // －　+
-        // ある要素の次の要素
-        $("#ruby + li").css("color", "green");
-        // －　~
-        // ある要素移行の全ての要素
-        $("#ruby ~ li").css("color", "yellow");
-        });
+//     －　取得した要素の順番
+// 　    －　:first, :last
+        // $("#compile li:last").css("color", "red");
+//  　   －　:even, :add
+        // $("#compile li:even").css("color", "red");
+// 　    －　:eq(index), :li(index), :gt(index)
+        // $("#compile li:eq(1)").css("color", "red");
+//     －　条件を満たす子要素の有無
+//     　－　:has(selector)
+        $("li:has(ul)").css("color", "red");
+//     －　内容の有無
+//     　－　:empty, :parent
+//     　－　:contains(text)
+        // $("#compile li:contains(C)").css("color", "red");
     </script>
 </body>
 </html>
 
 <!-- 
-    階層セレクタで要素を絞り込む
-    ・階層セレクタとは？
-    －　要素の親子関係で絞り込む
-    ・書き方
-    $("基本セレクタ　結合子　基本セレクタ")     ×("基本セレクタ" 結合子 "基本セレクタ")
-    ・結合子
-    －　（スペース）
-    －　>
-    －　+
-    －　~
+    フィルタで要素を絞り込む
+    ・フィルタとは
+    －　取得した要素をさらに絞り込むための条件
+    ・様々なフィルタ
+    －　取得した要素の順番
+　    －　:first, :last
+ 　   －　:even, :add   even：偶数
+　    －　:eq(index), :li(index), :gt(index)
+    －　条件を満たす子要素の有無
+    　－　:has(selector)
+    －　内容の有無
+    　－　:empty, :parent
+    　－　:contains(text)
     
  -->
