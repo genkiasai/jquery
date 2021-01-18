3b56b7d0c1c303403c0b40610e2372632745de00
genkiasai <baseballboy1025@gmail.com>
2021-01-19 07:02:50 +0900

    3-1

--- index.php
+++ index.php
@@ -6,26 +6,47 @@
     <title>jQuery</title>
 </head>
 <body>
-    <a href="http://google.co.jp">Google</a>
-    <a href="http://yahoo.co.jp" target="_blank">Yahoo</a>
-    <a href="http://bing.com" target="self">Bing</a>
+<div id="compile">
+        <h1 id="title1" class="title">コンパイル言語</h1>
+        <ul>
+            <li id="java">Java</li>
+            <li id="c">C
+                <ul>
+                    <li id="cpp">C++</li>
+                    <li id="cs">C#</li>
+                </ul>
+            </li>
+            <li id="objective-c">ObjectiveC</li>
+        </ul>
+    </div>
+    <div id="script">
+        <h1 id="title2" class="title">スクリプト言語</h1>
+        <ul>
+            <li id="js">JavaScript</li>
+            <li id="ruby">Ruby</li>
+            <li id="python">Python</li>
+            <li id="php">PHP</li>
+            <li id="perl">perl</li>
+        </ul>
+    </div>
     <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
     <script src="./jquery-3.5.1.min.js"></script>
     <script>
         $(function(){
-            // $("a[target]").css("color", "red");
-            // $("a[target='_blank']").css("color", "red");
-            // *='文字列'：その文字を含むもの
-            // $("a[href*='co.jp']").css("color", "red");
-            $("a[href*='co.jp'][target]").css("color", "red");
+            // prev, prevAll, next, nextAll
+            // $("#python").nextAll().css("color", "red");
+            // parent, parents, children
+            $("#c").children().css("color", "red");
         });
     </script>
 </body>
 </html>
 
 <!-- 
-    属性フィルタで要素を絞り込む
-    ・属性フィルタとは
-    －　条件を満たす属性や属性値で絞り込む
-    
+    メソッドを使った要素指定
+    ・jQueryの基本
+    －　$(), jQueryオブジェクト
+    －　$(selector).method
+    ・ある要素を起点として周囲の要素を取得する
+
  -->
