a457931e5b231108479b94bc41bd004e1f044371
genkiasai <baseballboy1025@gmail.com>
2021-01-22 06:41:16 +0900

    3-2

--- index.php
+++ index.php
@@ -6,47 +6,32 @@
     <title>jQuery</title>
 </head>
 <body>
-<div id="compile">
-        <h1 id="title1" class="title">コンパイル言語</h1>
-        <ul>
-            <li id="java">Java</li>
-            <li id="c">C
-                <ul>
-                    <li id="cpp">C++</li>
-                    <li id="cs">C#</li>
-                </ul>
-            </li>
-            <li id="objective-c">ObjectiveC</li>
-        </ul>
-    </div>
-    <div id="script">
-        <h1 id="title2" class="title">スクリプト言語</h1>
-        <ul>
-            <li id="js">JavaScript</li>
-            <li id="ruby">Ruby</li>
-            <li id="python">Python</li>
-            <li id="php">PHP</li>
-            <li id="perl">perl</li>
-        </ul>
-    </div>
+    <div id="title"><h1>Hello World!</h1></div>
+    <input id="form" type="text" value="hello">
     <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
     <script src="./jquery-3.5.1.min.js"></script>
     <script>
-        $(function(){
-            // prev, prevAll, next, nextAll
-            // $("#python").nextAll().css("color", "red");
-            // parent, parents, children
-            $("#c").children().css("color", "red");
-        });
+    $(function(){
+        // console.log($("#title").text());
+        // console.log($("#title").html());
+        // $("#title").text("こんにちは");//h1タグが消える
+        // $("#title").html("こんにちは");//h1タグが消える
+        // $("#title").html("<h1>こんにちは</h1>");
+        // $("#title").text("<h1>こんにちは</h1>");
+        console.log($("#form").val());//フォームの値取得
+        console.log($("#form").val("Hello World!"));//フォームの値書き換え
+    });
     </script>
 </body>
 </html>
 
 <!-- 
-    メソッドを使った要素指定
-    ・jQueryの基本
-    －　$(), jQueryオブジェクト
-    －　$(selector).method
-    ・ある要素を起点として周囲の要素を取得する
+    要素内のテキストの操作
 
- -->
+    ・要素内のテキストの操作
+    －　text()
+    －　html()
+    ・フォーム要素の値の操作
+    －　val()
+
+-->
